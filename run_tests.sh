d7105884137bef260446c84f36083b402cc0b0cd
Karol Duleba <mr.fuxi@gmail.com>
2015-03-27 23:27:26 +0000

    Include coverage in tests

--- run_tests.sh
+++ run_tests.sh
@@ -5,4 +5,4 @@ if [[ $1 == '-v' ]]; then
     v="-test.v"
 fi
 
-go test $v $(ls -d ./*/ | grep -v static ) | grep -v "no test files" | grep -v "martini"
+go test $v --cover $(ls -d ./*/ | grep -v static ) | grep -v "no test files" | grep -v "martini"
